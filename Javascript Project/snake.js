0fc016b3204f3c9fa90554c1a181516f8185706e
Suryansh Yadav <ysuryansh576@gmail.com>
2026-07-11 01:20:53 +0530

    Add files via upload

---
+++ Javascript Project/snake.js
@@ -0,0 +1,11 @@
+let contain = document.querySelector(".container")
+let love = document.querySelector("i")
+
+
+contain.addEventListener("dblclick", function () {
+  love.style.transform = "translate(-50%, -50%) scale(01)"
+})
+
+setTimeout(function () {
+  
+},2000)
